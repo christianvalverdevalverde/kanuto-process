8262ea4eba4cf0e28d73131a854c420ccd10664a
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2017-08-28 21:53:08 -0500

    Clases y archivos de utilerías para consumo de servicios JSON

---
+++ src/main/java/ec/nubesoft/kiosko/servicio/MovimientoService.java
@@ -0,0 +1,42 @@
+package ec.nubesoft.kiosko.servicio;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import ec.nubesoft.kiosko.util.Utileria;
+
+import java.util.Map;
+
+/**
+ * Movimiento service class
+ * @author carper
+ * 2017-08-28
+ */
+public class MovimientoService{
+	
+	public String doPost(){
+		String valor = null;
+
+		Client client = Client.create();
+
+		Map config = Utileria.getInstancia().getConfigMap();
+		if (config.size() > 0) {
+			String url = (String) config.get("MOVIMIENTOS_URL");
+
+			WebResource webResource = client.resource( url );
+
+			ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+
+			if (response.getStatus() != 200) 
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+			else
+				valor = response.getEntity(String.class);
+		}
+
+		return valor;	
+	}
+
+}
+
+
