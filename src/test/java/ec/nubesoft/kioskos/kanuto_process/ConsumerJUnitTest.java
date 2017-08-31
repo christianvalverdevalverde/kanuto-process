ff4971cc67d4cfab307e9406d18a195d4d07c5c2
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2017-08-31 11:59:05 -0500

    Clases para consumo JSON WS

--- src/test/java/ec/nubesoft/kioskos/kanuto_process/ConsumerJUnitTest.java
+++ src/test/java/ec/nubesoft/kioskos/kanuto_process/ConsumerJUnitTest.java
@@ -1,6 +1,8 @@
 package ec.nubesoft.kioskos.kanuto_process;
 
 import ec.nubesoft.kiosko.servicio.ConsumerService;
+import ec.nubesoft.kiosko.servicio.MovimientoBean;
+import java.math.BigDecimal;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,8 +18,15 @@ public class ConsumerJUnitTest{
 	@Test
 	public void testPOST(){
 		//System.out.println("Hello World");
-		String output = new ConsumerService().doPost();
+	
+		MovimientoBean m = new MovimientoBean();
+		m.setMoneda_01(BigDecimal.ZERO);
+		m.setMoneda_05(new BigDecimal("0.05"));
+		m.setMoneda_10(new BigDecimal("0.40"));
+		m.setMac("aa-bb-cc-dd-ee");
+		m.setNumeroOrden(0);  //usa cero para movimientos manuales.
 
+		String output = "" + new ConsumerService().doMovimientoPost(m);
 		System.out.println(output);
 
 	}
