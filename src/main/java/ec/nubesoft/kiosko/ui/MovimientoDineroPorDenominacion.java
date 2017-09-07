72a5230d230ede13c78969e50ed5bb3dff8ac05c
Knoppix User <carlosjulioperez@gmail.com>
2017-09-07 18:55:30 -0500

    El formulario de movimieto por defecto es I)ngreso

--- src/main/java/ec/nubesoft/kiosko/ui/MovimientoDineroPorDenominacion.java
+++ src/main/java/ec/nubesoft/kiosko/ui/MovimientoDineroPorDenominacion.java
@@ -1,20 +1,35 @@
 package ec.nubesoft.kiosko.ui;
 
 import org.zkoss.zk.ui.util.GenericForwardComposer;
-import org.zkoss.zul.Intbox;
 import org.zkoss.zul.Label;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Window;
 import org.zkoss.zk.ui.Component;
 
 /**
  * Clase MovimientoDineroPorDenominacion para registrar ingresos y egresos de dinero
  * @author carper
- * 2017-09-04
+ * 2017-09-04, 07
  */ 
 public class MovimientoDineroPorDenominacion extends GenericForwardComposer {
 
+	private Window win;
 	private Label lblParametro;
+	private String tipo;
 
 	public void doAfterCompose(Component comp) throws Exception {
+
+		super.doAfterCompose(comp);
+
+		try{
+			tipo = lblParametro.getValue();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			tipo = "I";
+		}
+
+		win.setTitle( (tipo.equals("I") ? "Ingreso": "Egreso") + " " + win.getTitle() );  
 	}
 
 }
